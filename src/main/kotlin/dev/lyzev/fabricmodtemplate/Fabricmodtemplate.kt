02907e003ad9f13fb4c97f81248305b2f98d1a45
Lyzev <lyzev.info@gmail.com>
2024-12-31 18:42:54 +0100

    Initial commit

---
+++ src/main/kotlin/dev/lyzev/fabricmodtemplate/Fabricmodtemplate.kt
@@ -0,0 +1,9 @@
+package dev.lyzev.fabricmodtemplate
+
+import net.fabricmc.api.ModInitializer
+
+class Fabricmodtemplate : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
---
+++ src/main/kotlin/dev/lyzev/fabricmodtemplate/client/FabricmodtemplateClient.kt
@@ -0,0 +1,9 @@
+package dev.lyzev.fabricmodtemplate.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class FabricmodtemplateClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
